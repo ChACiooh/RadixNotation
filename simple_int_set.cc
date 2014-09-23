a99e32c86f5e28ab8b29eb982be87a318c39061f
Kurumi_Tokisaki <y007com@naver.com>
2014-09-23 16:38:06 +0900

    need to edit

--- simple_int_set.cc
+++ simple_int_set.cc
@@ -7,10 +7,10 @@ using namespace std;
 
 class calSet
 {
-	int InputeIntSet(int* buf, int buflen);  // Get one set input { ... } from cin.
-	int IntersectIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
-	int UnionIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
-	int DifferenceIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
+	int* InputIntSet(int* buf, int buflen);  // Get one set input { ... } from cin.
+	int* IntersectIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
+	int* UnionIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
+	int* DifferenceIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
 }
 
 int main()
@@ -20,15 +20,16 @@ int main()
 		int set1[MAX_SIZE], set2[MAX_SIZE], set_ret[MAX_SIZE];
 		int *buf = new int[10000];
 		int len = 0;
-		while(scanf("%d", &buf[len++])!=EOF);
 		calSet cs = new calSet;
-		set1 = cs.InputeIntSet(buf, len);
-		set2 = cs.InputeIntSet(buf, len);
+		while(scanf("%d", &buf[len++])!=EOF)
+		{
+			
+		}
 	}
 	return 0;
 }
 
-int InputeIntSet(int* buf, int buflen)
+int* InputeIntSet(int* buf, int buflen)
 {
 	
 }
