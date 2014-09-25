68fd925f4b0491240b722ec51db00fbad7894721
Kurumi_Tokisaki <y007com@naver.com>
2014-09-25 19:15:10 +0900

    need to edit

--- makefile
+++ makefile
@@ -1,19 +1,4 @@
-all : radix_notation draw_points simple_calculator
-radix_notation: radix_notation.o
-	g++ -o radix_notation radix_notation.o
-radix_notation.o: radix_notation.cc
-	g++ -c radix_notation.cc
-
-draw_points: draw_points.o
-	g++ -o draw_points draw_points.o
-draw_points.o: draw_points.cc
-	g++ -c draw_points.cc
-
-simple_calculator: simple_calculator.o
-	g++ -o simple_calculator simple_calculator.o
-simple_calculator.o: simple_calculator.cc
-	g++ -c simple_calculator.cc
-
-clean:
-	rm radix_notation.o draw_points.o simple_calculator.o
+all: simple_int_set
+simple_int_set: simple_int_set.cc
+	g++ -o simple_int_set simple_int_set.cc
 
--- simple_int_set.cc
+++ simple_int_set.cc
@@ -1,35 +1,190 @@
+/***
+ *** This program is to operate two sets.
+ ***/
 #include <iostream>
-#include <string>
 #include <cstdio>
+#include <string>
+#include <algorithm>
+#include <queue>
+
 #define MAX_SIZE 100
+#define MAX_NUM(x,y)	((x)>(y))?(x):(y)
+#define MIN_NUM(x,y)	((x)<(y))?(x):(y)
 
 using namespace std;
 
-class calSet
-{
-	int* InputIntSet(int* buf, int buflen);  // Get one set input { ... } from cin.
-	int* IntersectIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
-	int* UnionIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
-	int* DifferenceIntSet(const int* set1, int set1size, const int* set2, int set2size, int* set_ret, int buflen);
-}
+queue<int> myQ;	//global STL variable is so useful!
+
+int InputIntSet(int *set, int len);
+int IntersectIntSet(int *set1, int len1, int *set2, int len2, int* res);
+int UnionIntSet(int *set1, int len1, int *set2, int len2, int *res);
+int DifferenceIntSet(int *set1, int len1, int *set2, int len2, int *res);
 
 int main()
 {
-	do
+	char *buffer[1000];
+	int i = 0, setCnt = 0, len1, len2, res_size;
+	int *set1, *set2, *result;
+	char operator_;
+
+	// process for input
+	while(scanf("%s", buffer[i]) != EOF || setCnt < 3)
 	{
-		int set1[MAX_SIZE], set2[MAX_SIZE], set_ret[MAX_SIZE];
-		int *buf = new int[10000];
-		int len = 0;
-		calSet cs = new calSet;
-		while(scanf("%d", &buf[len++])!=EOF)
+		if(i == 0 && buffer[i][0] != '{')	return 0;
+		//if(buffer[i][0] == '\n')	break;
+		if(buffer[i][0] == '{')
 		{
-			
+			setCnt++;
+			i++;
+			continue;
 		}
+		else if(buffer[i][0] == '}')
+		{
+			switch(setCnt)
+			{
+				case 1:
+					len1 = InputIntSet(set1, myQ.size());
+					break;
+				case 2:
+					len2 = InputIntSet(set2, myQ.size());
+					break;
+				default:
+					break;
+			}
+			i++;
+			continue;
+		}
+		else if(buffer[i][0] == '+' || buffer[i][0] == '-' || buffer[i][0] == '*')
+		{
+			operator_ = buffer[i][0];
+			i++;
+			continue;
+		}
+		int num;
+		sscanf(buffer[i], "%d", &num);
+		i++;
+		myQ.push(num);
 	}
+	
+	// operating
+	switch(operator_)
+	{
+		case '+':
+			res_size = UnionIntSet(set1, len1, set2, len2, result);
+			break;
+		case '*':
+			res_size = IntersectIntSet(set1, len1, set2, len2, result);
+			break;
+		case '-':
+			res_size = IntersectIntSet(set1, len1, set2, len2, result);
+			break;
+		default:
+			break;
+	}
+	printf("{ ");
+	for(i = 0; i < res_size; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << "}" << endl;
 	return 0;
 }
 
-int* InputeIntSet(int* buf, int buflen)
+int InputIntSet(int *set, int len)
 {
-	
+	int *temp = new int[len];
+	int i = 0;
+	while(!myQ.empty())
+	{
+		temp[i++] = myQ.front();
+		myQ.pop();
+	}
+
+	set = new int[len];
+	i = 0;
+	while(!myQ.empty())
+	{
+		int now = temp[i];
+		if(i>0 && set[i-1] == now)	continue;
+		// same elements will be ignored.
+		set[i] = now;
+		i++;
+	}
+	sort(set, set+i);
+	return i;
+}
+
+int IntersectIntSet(int *set1, int len1, int *set2, int len2, int *res)
+{
+	int res_size = MAX_NUM(len1, len2), cnt = 0;
+	res = new int[res_size];
+	int i, j;
+	i = j = 0;
+	while(i<len1)
+	{
+		int now = set1[i];
+		j = 0;
+		while(j<len2)
+		{
+			if(now == set2[j])
+			{
+				cnt++;
+				res[i] = now;
+				break;
+				// if one element is discovered, then break.
+				// because same elements were ignored in the set.
+			}
+			j++;
+		}
+		i++;
+	}
+	return cnt;
+}
+
+int UnionIntSet(int *set1, int len1, int *set2, int len2, int *res)
+{
+	int res_size = len1+len2, cnt = 0, i, j;
+	res = new int[res_size];
+	i = j = 0;
+	while(i < len1 || j < len2)
+	{
+		if(i >= len1)	res[cnt++] = set2[j++];
+		else if(j >= len2)	res[cnt++] = set1[i++];
+		else
+		{			
+			if(set1[i] < set2[j])
+			{
+				res[cnt++] = set1[i++];
+			}
+			else
+			{
+				res[cnt++] = set2[j++];
+			}
+		}
+	}
+	return cnt;
+}
+
+int DeferenceIntSet(int *set1, int len1, int *set2, int len2, int *res)
+{
+	int res_size = MAX_NUM(len1, len2), cnt = 0;
+	res = new int[res_size];
+	int *intersect = new int[res_size];
+	int len3 = IntersectIntSet(set1, len1, set2, len2, intersect);
+	for(int i=0;i<len1;i++)
+	{
+		int now = set1[i];
+		bool find = false;
+		for(int j=0;j<len3;j++)
+		{
+			if(now == intersect[j])
+			{
+				find = true;
+				break;
+			}
+		}
+		if(!find)
+			res[cnt++] = now;
+	}
+	return cnt;
 }
