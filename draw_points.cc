6afe5924b21d70d1c22990308212f0474f04d792
Kurumi_Tokisaki <y007com@naver.com>
2014-09-25 19:14:47 +0900

    ver3

--- draw_points.cc
+++ draw_points.cc
@@ -1,29 +1,39 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-char arr[1000][1000];
 
 int main()
 {
 	int x, y, max_x, max_y;
 	max_x = max_y = 0;
+	vector< pair<int, int> > points;
 	while(true)
 	{
 		cin >> x >> y;
 		if(x<0 || y<0)	break;
 		if(max_x<x)	max_x = x;
 		if(max_y<y)	max_y = y;
-
+		points.push_back(make_pair(x, y));
 		for(int i=0;i<=max_y;i++)
 		{
 			for(int j=0;j<=max_x;j++)
 			{
-				if(j == x && y == i)	arr[i][j] = '*';
-				if(arr[i][j] == '*')	continue;
-				else	arr[i][j] = '.';
+				bool marked = false;
+				for(int k=0;k<points.size();k++)
+				{
+					if(j == points[k].first && i == points[k].second)
+					{
+						marked = true;
+						cout << "*";
+						break;
+					}
+				}
+				if(marked)	continue;
+				else cout << ".";
 			}
-			cout << arr[i] << endl;
+			cout << endl;
 		}
 	}
 	return 0;
